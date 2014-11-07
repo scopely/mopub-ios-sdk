85d5216e172813a4ec2253fade76e0371f11e3f2
odyth <jstofle@gmail.com>
2014-11-07 13:00:14 -0800

    fixed bug in mopub with iAd interstitials not showing on iOS 8 iPhone

--- AdNetworkSupport/iAd/MPiAdInterstitialCustomEvent.m
+++ AdNetworkSupport/iAd/MPiAdInterstitialCustomEvent.m
@@ -8,17 +8,25 @@
 #import "MPiAdInterstitialCustomEvent.h"
 #import "MPInstanceProvider.h"
 
-@interface MPInstanceProvider (iAdInterstitials)
-
-- (ADInterstitialAd *)buildADInterstitialAd;
+@interface WBiAdViewController : UIViewController
 
 @end
 
-@implementation MPInstanceProvider (iAdInterstitials)
+@implementation WBiAdViewController
 
-- (ADInterstitialAd *)buildADInterstitialAd
+-(instancetype)init
 {
-    return [[ADInterstitialAd alloc] init];
+    self = [super init];
+    if(self)
+    {
+        self.extendedLayoutIncludesOpaqueBars = YES;
+    }
+    return self;
+}
+
+-(BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 @end
@@ -28,18 +36,25 @@
 @interface MPiAdInterstitialCustomEvent ()
 
 @property (nonatomic, strong) ADInterstitialAd *iAdInterstitial;
-@property (nonatomic, assign) BOOL isOnScreen;
+@property (nonatomic, strong) WBiAdViewController *adViewController;
 
 @end
 
 @implementation MPiAdInterstitialCustomEvent
 
-@synthesize iAdInterstitial = _iAdInterstitial;
-@synthesize isOnScreen = _isOnScreen;
-
 - (void)requestInterstitialWithCustomEventInfo:(NSDictionary *)info
 {
-    self.iAdInterstitial = [[MPInstanceProvider sharedProvider] buildADInterstitialAd];
+    if(NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_6_1)
+    {
+        NSError *error = [NSError errorWithDomain:WBAdSDKDomain code:0 userInfo:@{
+                                                                                  NSLocalizedDescriptionKey : @"iAd is only available on iOS 7+",
+                                                                                  NSLocalizedFailureReasonErrorKey : @"Os not supported"
+                                                                                  }];
+        [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:error];
+        return;
+    }
+    
+    self.iAdInterstitial = [[ADInterstitialAd alloc] init];
     self.iAdInterstitial.delegate = self;
 }
 
@@ -50,12 +65,47 @@
 
 - (void)showInterstitialFromRootViewController:(UIViewController *)controller {
     // ADInterstitialAd throws an exception if we don't check the loaded flag prior to presenting.
-    if (self.iAdInterstitial.loaded) {
+    if (self.iAdInterstitial.loaded)
+    {
         [self.delegate interstitialCustomEventWillAppear:self];
-        [self.iAdInterstitial presentFromViewController:controller];
-        self.isOnScreen = YES;
-        [self.delegate interstitialCustomEventDidAppear:self];
-    } else {
+        self.adViewController = [[WBiAdViewController alloc] init];
+        [controller presentViewController:self.adViewController animated:YES completion:^{
+            [self.delegate interstitialCustomEventDidAppear:self];
+        }];
+        [self.iAdInterstitial presentInView:self.adViewController.view];
+        
+        
+        UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [closeButton addTarget:self action:@selector(close:) forControlEvents:UIControlEventTouchUpInside];
+        
+        CGFloat scale = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? 1 : 1.5;
+        closeButton.frame = CGRectMake(0, 0, 40 * scale, 40 * scale);
+        
+        UIView *fill = [[UIView alloc] initWithFrame:CGRectMake(11 * scale, 11 * scale, 18 * scale, 18 * scale)];
+        fill.userInteractionEnabled = NO;
+        fill.layer.cornerRadius = CGRectGetWidth(fill.frame)/2;
+        fill.backgroundColor = [UIColor whiteColor];
+        [closeButton addSubview:fill];
+        
+        UIView *circle = [[UIView alloc] initWithFrame:CGRectMake(10 * scale, 10 * scale, 20 * scale, 20 * scale)];
+        circle.userInteractionEnabled = NO;
+        circle.layer.cornerRadius = CGRectGetWidth(circle.frame)/2;
+        circle.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        circle.layer.borderWidth = 2 * scale;
+        
+        UILabel *x = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 20 * scale, 20 * scale)];
+        x.text = @"X";
+        x.font = [UIFont boldSystemFontOfSize:12 * scale];
+        x.textAlignment = NSTextAlignmentCenter;
+        x.textColor = [UIColor darkGrayColor];
+        [circle addSubview:x];
+        
+        [closeButton addSubview:circle];
+        
+        [self.adViewController.view addSubview:closeButton];
+    }
+    else
+    {
         CoreLogType(WBLogLevelError, WBLogTypeAdFullPage, @"Failed to show iAd interstitial: a previously loaded iAd interstitial now claims not to be ready.");
     }
 }
@@ -67,16 +117,24 @@
 
 - (void)interstitialAdDismissed
 {
-    if (self.isOnScreen) {
+    if (self.adViewController)
+    {
         [self.delegate interstitialCustomEventWillDisappear:self];
-        [self.delegate interstitialCustomEventDidDisappear:self];
-        self.isOnScreen = NO; //technically not necessary as iAd interstitials are single use
+        [self.adViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
+            [self.delegate interstitialCustomEventDidDisappear:self];
+        }];
     }
+}
+
+-(IBAction)close:(id)sender
+{
+    [self interstitialAdDismissed];
 }
 
 #pragma mark - <ADInterstitialAdDelegate>
 
-- (void)interstitialAdDidLoad:(ADInterstitialAd *)interstitialAd {
+- (void)interstitialAdDidLoad:(ADInterstitialAd *)interstitialAd
+{
     [self.delegate interstitialCustomEvent:self didLoadAd:self.iAdInterstitial];
 }
 
@@ -84,13 +142,14 @@
     [self.delegate interstitialCustomEvent:self didFailToLoadAdWithError:error];
 }
 
-- (void)interstitialAdDidUnload:(ADInterstitialAd *)interstitialAd {
-    // This method may be called whether the ad is on-screen or not. We only want to invoke the
-    // "disappear" callbacks if the ad is on-screen.
+- (void)interstitialAdDidUnload:(ADInterstitialAd *)interstitialAd
+{
     [self interstitialAdDismissed];
-
-    // ADInterstitialAd can't be shown again after it has unloaded, so notify the controller.
-    [self.delegate interstitialCustomEventDidExpire:self];
+    if(self.adViewController == nil)
+    {
+        // ADInterstitialAd can't be shown again after it has unloaded, so notify the controller.
+        [self.delegate interstitialCustomEventDidExpire:self];
+    }
 }
 
 - (BOOL)interstitialAdActionShouldBegin:(ADInterstitialAd *)interstitialAd
