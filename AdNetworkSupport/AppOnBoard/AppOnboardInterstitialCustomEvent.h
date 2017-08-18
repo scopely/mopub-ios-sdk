680f19c47ba1ed350170a53ed6e2f2d98fa6c5d8
nikhilcn <nikslash@yahoo.com>
2017-08-17 18:25:56 -0700

    AppOnboard Mopub adapter

---
+++ AdNetworkSupport/AppOnBoard/AppOnboardInterstitialCustomEvent.h
@@ -0,0 +1,13 @@
+//
+//  AppOnboardInterstitialCustomEvent.h
+//  AppOnboard
+//
+//  Created by Adam Piechowicz on 4/28/17.
+//  Copyright © 2017 App Onboard, Inc. All rights reserved.
+//
+
+#import "MPInterstitialCustomEvent.h"
+
+@interface AppOnboardInterstitialCustomEvent : MPInterstitialCustomEvent
+
+@end
---
+++ AdNetworkSupport/AppOnBoard/AppOnboardRewardedVideoCustomEvent.h
@@ -0,0 +1,13 @@
+//
+//  AppOnboardRewardedVideoCustomEvent.h
+//  AppOnboard
+//
+//  Created by Adam Piechowicz on 5/3/17.
+//  Copyright © 2017 App Onboard, Inc. All rights reserved.
+//
+
+#import "MPRewardedVideoCustomEvent.h"
+
+@interface AppOnboardRewardedVideoCustomEvent : MPRewardedVideoCustomEvent
+
+@end
